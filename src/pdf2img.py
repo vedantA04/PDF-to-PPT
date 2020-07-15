8e7957613dc68d0401ff03b67ad570d182b84fa5
vedantA04 <68198041+vedantA04@users.noreply.github.com>
2020-07-15 11:10:25 +0530

    Update pdf2img.py

--- src/pdf2img.py
+++ src/pdf2img.py
@@ -1,12 +1,15 @@
 from pdf2image import convert_from_path
+import os
 
 n = input("What is the name of the file?")
 def pdf2imgs(n):
     pages = convert_from_path("C:/Users/ANUP/Desktop/Maa_Sanskrit/Stories/"+n+"/"+n+".pdf" , 500)
-    outdir = 'Images/'
+    outdir = 'Images'
     x = 1
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
     for page in pages:
-        page.save('{}Picture{}.png'.format(outdir,x), 'PNG')
+        page.save('{}/Picture{}.png'.format(outdir,x), 'PNG')
         x += 1
 
 
