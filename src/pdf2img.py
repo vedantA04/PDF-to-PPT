62556c9534943ae976f1aa519543f61fe776bdc2
vedantA04 <68198041+vedantA04@users.noreply.github.com>
2020-07-15 11:11:47 +0530

    Update pdf2img.py

--- src/pdf2img.py
+++ src/pdf2img.py
@@ -3,7 +3,7 @@ import os
 
 n = input("What is the name of the file?")
 def pdf2imgs(n):
-    pages = convert_from_path("C:/Users/ANUP/Desktop/Maa_Sanskrit/Stories/"+n+"/"+n+".pdf" , 500)
+    pages = convert_from_path("C:/Users/ANUP/Desktop/PDF-to-PPT/"+n+".pdf" , 500)
     outdir = 'Images'
     x = 1
     if not os.path.exists(outdir):
