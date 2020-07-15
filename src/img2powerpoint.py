5ee45ec617f09d4c9721bf04733e3404ed021c94
vedantA04 <68198041+vedantA04@users.noreply.github.com>
2020-07-15 13:30:18 +0530

    Update img2powerpoint.py

--- src/img2powerpoint.py
+++ src/img2powerpoint.py
@@ -2,10 +2,9 @@ from pptx import Presentation
 from pptx.util import Inches
 import os
 import natsort
-from pdf2img import n
 import image
 
-def imgs2powerpoint():
+def imgs2powerpoint(outpath):
     prs = Presentation()
     img = image.Image("Images/Picture1.png")
     width = img.getWidth()
@@ -18,5 +17,4 @@ def imgs2powerpoint():
         slide = prs.slides.add_slide(blank_slide_layout)
         left = top = 0
         slide.shapes.add_picture("Images/{}".format(image), left, top, height = prs.slide_height, width = prs.slide_width)
-    prs.save("C:/Users/Your_User_Name/Desktop/PDF-to-PPT/"+n+".pptx")#Path for Windows 
-             #Path for Linux, "/home/your_user_name(in small letters)/Desktop/PDF-to-PPT/"
+    prs.save(outpath)
